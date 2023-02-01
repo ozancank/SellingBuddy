5e49cf3411ca1c217bde08aec6d9ebf519540bb8
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2023-02-01 16:24:14 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace EventBus.Base.Events;
+
+public class IntegrationEvent
+{
+    [JsonProperty]
+    public Guid Id { get; set; }
+
+    [JsonProperty]
+    public DateTime CreatedDate { get; set; }
+
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.Now;
+    }
+
+    [JsonConstructor]
+    public IntegrationEvent(Guid id, DateTime createdDate)
+    {
+        Id = id;
+        CreatedDate = createdDate;
+    }
+}
