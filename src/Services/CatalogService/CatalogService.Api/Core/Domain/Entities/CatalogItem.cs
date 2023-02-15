47d082c54272ea6a0db40b6c514fb9de9a2f1270
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2023-02-15 16:35:22 +0300

    .

--- src/Services/CatalogService/CatalogService.Api/Core/Domain/Entities/CatalogItem.cs
+++ src/Services/CatalogService/CatalogService.Api/Core/Domain/Entities/CatalogItem.cs
@@ -10,8 +10,12 @@ public class CatalogItem
     public decimal Price { get; set; }
     public string PictureFileName { get; set; }
     public string PictureUri { get; set; }
+    public int AvailableStock { get; set; }
+    public bool OnReorder { get; set; }
+
     public int CatalogTypeId { get; set; }
     public CatalogType CatalogType { get; set; }
+
     public int CatalogBrandId { get; set; }
     public CatalogBrand CatalogBrand { get; set; }
 }
