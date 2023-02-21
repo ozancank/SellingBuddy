1bb31b99676a0e13a81cbc6eb0b863b4f48e0748
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2023-02-21 16:45:53 +0300

    .

---
+++ src/Services/OrderService/OrderService.Infrastructure/Context/OrderDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.AggregateModels.BuyerAggregate;
+using OrderService.Domain.AggregateModels.OrderAggregate;
+using OrderService.Infrastructure.EntityConfigurations;
+
+namespace OrderService.Infrastructure.Context;
+
+public class OrderDbContext : DbContext
+{
+    public const string DEFAULT_SCHEMA = "ordering";
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<PaymentMethod> Payments { get; set; }
+    public DbSet<Buyer> Buyers { get; set; }
+    public DbSet<CardType> CardTypes { get; set; }
+    public DbSet<OrderStatus> OrderStatus { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new OrderEntityConfiguration());
+    }
+}
