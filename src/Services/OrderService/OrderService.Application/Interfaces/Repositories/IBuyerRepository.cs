139a8c2c221d90a0cb14465268275e0729b4d6bb
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2023-02-21 16:32:02 +0300

    .

---
+++ src/Services/OrderService/OrderService.Application/Interfaces/Repositories/IBuyerRepository.cs
@@ -0,0 +1,7 @@
+﻿using OrderService.Domain.AggregateModels.BuyerAggregate;
+
+namespace OrderService.Application.Interfaces.Repositories;
+
+public interface IBuyerRepository : IGenericRepository<Buyer>
+{
+}
---
+++ src/Services/OrderService/OrderService.Application/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace OrderService.Application;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddApplicationRegistration(this IServiceCollection services, Type startup)
+    {
+        var assm = Assembly.GetExecutingAssembly();
+
+        services.AddAutoMapper(assm);
+        services.AddMediatR(assm);
+
+        return services;
+    }
+}
